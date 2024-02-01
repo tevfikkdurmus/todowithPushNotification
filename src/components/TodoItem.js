36e34ffc4e119475b9a7d1529b4b846d8ffd5016
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-02-01 11:42:36 +0300

    added bell icon to todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
 const TodoItem = ({ data, deleteTodoItem, updateProperties }) => {
     return (
         <View style={styles.todoItem}>
-            <Text>{data.task}</Text>
+            <View style={{ flexDirection: "row", gap: 2, alignItems: "center" }}>
+                {data.alert != null && <MaterialCommunityIcons name="bell-ring" size={12} color="black" />}
+                <Text>{data.task}</Text>
+            </View>
             <View style={styles.buttonsContainer}>
                 <TouchableOpacity onPress={() => updateProperties(data.id)}>
                     <Entypo name="pencil" size={24} color="black" />
